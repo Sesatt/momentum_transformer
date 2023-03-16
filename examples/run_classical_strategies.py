5f013b747ff3a43de7427284d7171869c3cf5c97
Sesatt <67186636+Sesatt@users.noreply.github.com>
2023-03-16 08:53:09 +0100

    Update run_classical_strategies.py

--- examples/run_classical_strategies.py
+++ examples/run_classical_strategies.py
@@ -1,7 +1,7 @@
 import os
 from mom_trans.backtest import run_classical_methods
 
-INTERVALS = [(2012, y, y + 1) for y in range(2018, 2023)]
+INTERVALS = [(2012, y, y + 1) for y in range(2020, 2023)]
 
 REFERENCE_EXPERIMENT = "experiment_bitcoin_tft_cpnone_len252_notime_div_v1"
 
