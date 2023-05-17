503886f7bea9bb55c59f17d6c2f89f4a1d6ce4b1
Sesatt <67186636+Sesatt@users.noreply.github.com>
2023-05-17 18:13:34 +0200

    Update run_classical_strategies.py
    changed experiment name

--- examples/run_classical_strategies.py
+++ examples/run_classical_strategies.py
@@ -3,7 +3,7 @@ from mom_trans.backtest import run_classical_methods
 
 INTERVALS = [(1990, y, y + 1) for y in range(2016, 2023 -1)]
 
-REFERENCE_EXPERIMENT = "experiment_all_industries_tft_cpnone_len252_notime_div_v1"
+REFERENCE_EXPERIMENT = "experiment_sp500_tft_cpnone_len252_notime_div_v1"
 
 features_file_path = os.path.join(
     "data",
