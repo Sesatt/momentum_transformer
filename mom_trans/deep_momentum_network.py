9314b5bd80f83c85175acd444e93518eec984eb0
Sesatt <67186636+Sesatt@users.noreply.github.com>
2023-06-07 18:35:23 +0200

    Update deep_momentum_network.py

--- mom_trans/deep_momentum_network.py
+++ mom_trans/deep_momentum_network.py
@@ -595,6 +595,7 @@ class TransformerDeepMomentumNetworkModel(DeepMomentumNetworkModel):
         return static_inputs[0], static_inputs[1] 
     
     def PositionEncoding(seq_len, output_dim, n=10000):
+        print(type(seq_len), type(output_dim))
         P = np.zeros((int(seq_len), int(output_dim)))
         for k in range(seq_len):
             for i in np.arange(int(output_dim/2)):
