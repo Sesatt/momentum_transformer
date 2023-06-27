15c33419eecfce9d3ffb6323d7f1d047facd12f0
Sesatt <67186636+Sesatt@users.noreply.github.com>
2023-06-27 13:25:48 +0200

    Update run_classical_strategies.py

--- examples/run_classical_strategies.py
+++ examples/run_classical_strategies.py
@@ -1,13 +1,14 @@
 import os
 from mom_trans.backtest import run_classical_methods
 
-INTERVALS = [(1990, y, y + 1) for y in range(2016, 2023 -1)]
 
-REFERENCE_EXPERIMENT = "experiment_sp500_tft_cpnone_len252_notime_div_v1"
+INTERVALS = [(1990, y, min(y + 5, 2023 -1)) for y in range(2010, 2023 -1, 5)]
+
+REFERENCE_EXPERIMENT = "experiment_sp500_tsmom_tft_cpnone_len252_notime_div_v1"
 
 features_file_path = os.path.join(
     "data",
-    "quandl_cpd_nonelbw.csv",
+    "quandl_cpd_nonelbw_tsmom.csv",
 )
 
 run_classical_methods(features_file_path, INTERVALS, REFERENCE_EXPERIMENT)
