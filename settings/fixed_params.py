d08fc9d2614e12286732c52f85944890c3d95166
Sesatt <67186636+Sesatt@users.noreply.github.com>
2023-07-05 08:16:31 +0200

    Update fixed_params.py

--- settings/fixed_params.py
+++ settings/fixed_params.py
@@ -3,7 +3,7 @@ MODLE_PARAMS = {
     "total_time_steps": 252,
     "early_stopping_patience": 15,
     "multiprocessing_workers": 32,
-    "num_epochs": 300,
+    "num_epochs": 1,
     "fill_blank_dates": False,
     "split_tickers_individually": True,
     "random_search_iterations": 1,
