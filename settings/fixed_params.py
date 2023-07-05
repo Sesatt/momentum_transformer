94b9302312365cbc772f5fabefd19b4f9c1c517e
Sesatt <67186636+Sesatt@users.noreply.github.com>
2023-07-05 08:16:54 +0200

    Update fixed_params.py

--- settings/fixed_params.py
+++ settings/fixed_params.py
@@ -6,7 +6,7 @@ MODLE_PARAMS = {
     "num_epochs": 1,
     "fill_blank_dates": False,
     "split_tickers_individually": True,
-    "random_search_iterations": 1,
+    "random_search_iterations": 2,
     "evaluate_diversified_val_sharpe": True,
     "train_valid_ratio": 0.80,
     "time_features": False,
