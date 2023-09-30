fc5e4424729efb7a0de426fa96e9c408decace9d
Sesatt <67186636+Sesatt@users.noreply.github.com>
2023-09-30 20:34:42 +0200

    Revert "Update run_dmn_experiment.py"
    This reverts commit 8b2a778f78aaa196f23343e1ac60b66bcf1a32fc.

--- examples/run_dmn_experiment.py
+++ examples/run_dmn_experiment.py
@@ -17,7 +17,7 @@ TRAIN_VALID_RATIO = 0.80
 TIME_FEATURES = False
 FORCE_OUTPUT_SHARPE_LENGTH = None
 EVALUATE_DIVERSIFIED_VAL_SHARPE = True
-NAME = "experiment_sp500_tsmom_final_2022"
+NAME = "experiment_sp500_tsmom_final"
 TRANSACTION_COSTS = None
 
 def main(
